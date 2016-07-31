2e38676cbce526814c42f31f86115a4200f56dd4
rakesh.katti <rakesh.katti@flipkart.com>
2016-07-31 20:50:10 +0530

    Commit production webpack config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,52 +1,52 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
 const sassLoaders = [
-  'css-loader',
-  'postcss-loader',
-  'sass-loader?includePaths[]=' + path.resolve(__dirname, './app')
+    'css-loader',
+    'postcss-loader',
+    'sass-loader?includePaths[]=' + path.resolve(__dirname, './app')
 ]
 const node_modules_dir = path.resolve(__dirname, 'node_modules');
 
 const config = {
-  entry: {
-    app: path.resolve(__dirname, 'app/app.js')
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: "app.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ["react-hot", "babel-loader"],
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
-      },
-      {
-        test: /\.html$/,
-        loader: "file?name=[name].[ext]",
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin('[name].css')
-  ],
-  resolve: {
-    extensions: ['', '.js', '.scss'],
-    modulesDirectories: ['app', 'node_modules']
-  }
+    entry: {
+        app: path.resolve(__dirname, 'app/app.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "app.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ["react-hot", "babel-loader"],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+            },
+            {
+                test: /\.html$/,
+                loader: "file?name=[name].[ext]",
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css')
+    ],
+    resolve: {
+        extensions: ['', '.js', '.scss'],
+        modulesDirectories: ['app', 'node_modules']
+    }
 };
 
 module.exports = config;
